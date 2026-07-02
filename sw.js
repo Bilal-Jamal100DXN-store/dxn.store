6f7acff0f2c66ee9e9ec2b87d2beebbf02915b60
Bilal-Jamal100DXN-store <BilalJamalJassim100@gmail.com>
2026-07-03 00:54:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dxn-store-v26'; // زيدنا الإصدار لـ v26 علمود يمسح أي خربطة قديمة بجهازك
+const CACHE_NAME = 'dxn-store-v27'; // زيدنا الإصدار لـ v26 علمود يمسح أي خربطة قديمة بجهازك
 const assets = [
   './',
   './manifest.json',
